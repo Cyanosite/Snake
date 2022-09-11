109bb937530ffd59d0f0af5f50c8a283d8d36722
Cyanosite <zsomborszenyan@gmail.com>
2022-09-11 12:19:42 +0200

    optimized imports

--- src/GamePanel/GamePanel.java
+++ src/GamePanel/GamePanel.java
@@ -4,7 +4,10 @@ import Coordinate.Coordinate;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Random;
 
